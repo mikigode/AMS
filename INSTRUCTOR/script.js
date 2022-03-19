c8e02959372af04ada6dcfbe3405486f41332564
MikiGode <82653151+mikigode@users.noreply.github.com>
2022-03-19 17:28:40 +0300

    Add files via upload

---
+++ INSTRUCTOR/script.js
@@ -0,0 +1,30 @@
+const wrapper = document.getElementById('page__video')
+const card = document.querySelector('.card-body')
+const audio = document.getElementById("audio")
+const scanner = new Instascan.Scanner({ video: wrapper });
+console.log(wrapper)
+Instascan.Camera.getCameras().then(function (cameras) {
+    console.log(cameras)
+    if (cameras[1]) {
+        scanner.start(cameras[1]);
+    } else if (cameras[0]) {
+        scanner.start(cameras[0]);
+    } else {
+        console.error('No cameras found.');
+    }
+}).catch(function (e) {
+    console.error(e);
+});
+
+scanner.addListener('scan', function (content) {
+    const data = JSON.parse(content)
+
+    audio.play()
+
+    wrapper.style.display = 'none'
+
+    card.classList.remove('scanning')
+    card.innerHTML = '' +
+        '<div class="page__name">Welcome, ' + data.name + '.</div>' +
+        '<div class="page__date">Attend on : ' + new Date(data.date).toLocaleString() + '</div>'
+});
